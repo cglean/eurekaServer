e9eadcaaf06c3d0586b32bb6c8b3d31c69773dcd
Murtaza Goga <murtaza.goga@capgemini.com>
2016-08-23 16:03:10 -0400

    Prepare service registry for local dev

---
+++ src/test/java/com/cglean/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,18 @@
+package com.cglean.registry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.cglean.registry.ServiceRegistryApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ServiceRegistryApplication.class)
+public class ServiceRegistryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
